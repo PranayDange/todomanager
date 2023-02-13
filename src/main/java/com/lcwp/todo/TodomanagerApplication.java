3e97c43418380f517d3247b700bb4fd6562d06d4
pranay.dange <pranay.dange@paycraftsol.com>
2023-02-13 15:40:41 +0530

    first_commit

---
+++ src/main/java/com/lcwp/todo/TodomanagerApplication.java
@@ -0,0 +1,13 @@
+package com.lcwp.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodomanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodomanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lcwp/todo/TodomanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcwp.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodomanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
